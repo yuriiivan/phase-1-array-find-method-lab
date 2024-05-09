bd892a8690344e1939d084d533a9106a3860d85f
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-09 09:50:31 -0400

    Add func to return wins

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+const superbowlWin = (record) => {
+  const winRecord = record.find(item => item.result === 'W')
+  return winRecord === undefined ? undefined : winRecord.year
+}
